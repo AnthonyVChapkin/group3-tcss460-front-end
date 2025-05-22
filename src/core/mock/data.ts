1bc8dfb303b48b114d5d712578acdce558dc160f
Anthony Chapkin <anthonyvchapkin@gmail.com>
2025-05-22 14:27:39 -0700

    Create a books array with some mock data

---
+++ src/core/mock/data.ts
@@ -0,0 +1,126 @@
+import { IBook } from '../model/book.model';
+
+const books: IBook[] = [
+  {
+    isbn13: 9780439023480,
+    authors: 'Suzanne Collins',
+    publication: 2008,
+    original_title: 'The Hunger Games',
+    title: 'The Hunger Games (The Hunger Games, #1)',
+    ratings: {
+      average: 4.34,
+      count: 4780653,
+      rating_1: 66715,
+      rating_2: 127936,
+      rating_3: 560092,
+      rating_4: 1481305,
+      rating_5: 2706317
+    },
+    icons: {
+      large: 'https://images.gr-assets.com/books/1447303603m/2767052.jpg',
+      small: 'https://images.gr-assets.com/books/1447303603s/2767052.jpg'
+    }
+  },
+  {
+    isbn13: 9780316015840,
+    authors: 'Stephenie Meyer',
+    publication: 2005,
+    original_title: 'Twilight',
+    title: 'Twilight (Twilight, #1)',
+    ratings: {
+      average: 3.57,
+      count: 3866839,
+      rating_1: 456191,
+      rating_2: 436802,
+      rating_3: 793319,
+      rating_4: 875073,
+      rating_5: 1355439
+    },
+    icons: {
+      large: 'https://images.gr-assets.com/books/1361039443m/41865.jpg',
+      small: 'https://images.gr-assets.com/books/1361039443s/41865.jpg'
+    }
+  },
+  {
+    isbn13: 9780061120080,
+    authors: 'Harper Lee',
+    publication: 1960,
+    original_title: 'To Kill a Mockingbird',
+    title: 'To Kill a Mockingbird',
+    ratings: {
+      average: 4.25,
+      count: 3198671,
+      rating_1: 60427,
+      rating_2: 117415,
+      rating_3: 446835,
+      rating_4: 1001952,
+      rating_5: 1714267
+    },
+    icons: {
+      large: 'https://images.gr-assets.com/books/1361975680m/2657.jpg',
+      small: 'https://images.gr-assets.com/books/1361975680s/2657.jpg'
+    }
+  },
+  {
+    isbn13: 9780743273560,
+    authors: 'F. Scott Fitzgerald',
+    publication: 1925,
+    original_title: 'The Great Gatsby',
+    title: 'The Great Gatsby',
+    ratings: {
+      average: 3.89,
+      count: 2683664,
+      rating_1: 86236,
+      rating_2: 197621,
+      rating_3: 606158,
+      rating_4: 936012,
+      rating_5: 947718
+    },
+    icons: {
+      large: 'https://images.gr-assets.com/books/1490528560m/4671.jpg',
+      small: 'https://images.gr-assets.com/books/1490528560s/4671.jpg'
+    }
+  },
+  {
+    isbn13: 9780525478810,
+    authors: 'John Green',
+    publication: 2012,
+    original_title: 'The Fault in Our Stars',
+    title: 'The Fault in Our Stars',
+    ratings: {
+      average: 4.26,
+      count: 2346404,
+      rating_1: 47994,
+      rating_2: 92723,
+      rating_3: 327550,
+      rating_4: 698471,
+      rating_5: 1311871
+    },
+    icons: {
+      large: 'https://images.gr-assets.com/books/1360206420m/11870085.jpg',
+      small: 'https://images.gr-assets.com/books/1360206420s/11870085.jpg'
+    }
+  },
+  {
+    isbn13: 9780618260300,
+    authors: 'J.R.R. Tolkien',
+    publication: 1937,
+    original_title: 'The Hobbit or There and Back Again',
+    title: 'The Hobbit',
+    ratings: {
+      average: 4.25,
+      count: 2071616,
+      rating_1: 46023,
+      rating_2: 76784,
+      rating_3: 288649,
+      rating_4: 665635,
+      rating_5: 1119718
+    },
+    icons: {
+      large: 'https://images.gr-assets.com/books/1372847500m/5907.jpg',
+      small: 'https://images.gr-assets.com/books/1372847500s/5907.jpg'
+    }
+  }
+];
+
+export default books;
