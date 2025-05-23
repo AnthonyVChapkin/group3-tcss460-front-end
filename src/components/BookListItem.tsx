981de8e94213f4271cb48841c502923ea80cb1b9
Anthony Chapkin <anthonyvchapkin@gmail.com>
2025-05-22 20:17:21 -0700

    Make page to view a list of books show the isbn and cover of each book

--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -1,20 +1,43 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import CommentsDisabledIcon from '@mui/icons-material/CommentsDisabled';
-import { IconButton, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
-
+import { Avatar, Box, IconButton, ListItem, ListItemAvatar, ListItemText, Rating, Typography } from '@mui/material';
 import { IBook } from '../core/model/book.model';
-//import PriorityAvatar from 'components/Priority'; // Make cover component I guess
 
 export function BookListItem({ book, onDelete }: { book: IBook; onDelete: (isbn13: number) => void }) {
   return (
     <ListItem
+      alignItems="flex-start"
       secondaryAction={
         <IconButton edge="end" aria-label="delete" onClick={() => onDelete(book.isbn13)}>
           <DeleteIcon />
         </IconButton>
       }
     >
-      <ListItemText primary={book.title} secondary={book.authors} secondaryTypographyProps={{ color: 'gray' }} />
+      <ListItemAvatar>
+        <Avatar alt={book.title} src={book.icons?.small || book.icons?.large} variant="square" sx={{ width: 56, height: 84, mr: 2 }} />
+      </ListItemAvatar>
+
+      <ListItemText
+        primary={
+          <Box>
+            <Typography variant="subtitle1" fontWeight="bold">
+              {book.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              by {book.authors}
+            </Typography>
+          </Box>
+        }
+        secondary={
+          <Box mt={1}>
+            <Typography variant="body2">ISBN: {book.isbn13}</Typography>
+            <Box display="flex" alignItems="center" gap={1}>
+              <Rating name="average-rating" value={book.ratings.average} precision={0.1} readOnly size="small" />
+              <Typography variant="body2">({book.ratings.average.toFixed(2)})</Typography>
+            </Box>
+          </Box>
+        }
+      />
     </ListItem>
   );
 }
