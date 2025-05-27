3239749f8bf107bc66dfd1e49a2d585b96d949a6
zluqman206 <zluqman@uw.edu>
2025-05-26 23:27:02 -0700

    c

--- src/sections/auth/auth-forms/AuthRegister.tsx
+++ src/sections/auth/auth-forms/AuthRegister.tsx
@@ -69,7 +69,7 @@ export default function AuthRegister({ providers, csrfToken }: any) {
           password: Yup.string()
             .required('Password is required')
             .test('no-leading-trailing-whitespace', 'Password cannot start or end with spaces', (value) => value === value.trim())
-            .max(10, 'Password must be less than 10 characters')
+            .min(7, 'Password must be greater than 7 characters')
         })}
         onSubmit={async (values, { setErrors, setSubmitting }) => {
           const trimmedEmail = values.email.trim();
